43ee7b445235593cb055b34441aa5a4fe55a1182
EC2 Default User <ec2-user@ip-172-31-6-80.ap-southeast-2.compute.internal>
2026-04-12 08:14:48 +0000

    commit

--- apps/backend/prisma/db.ts
+++ apps/backend/prisma/db.ts
@@ -1,4 +1,4 @@
-import { PrismaClient } from "../src/generated/prisma/client";
+import { PrismaClient } from "@prisma/client";
 import { PrismaLibSql } from "@prisma/adapter-libsql";
 import path from "path";
 
